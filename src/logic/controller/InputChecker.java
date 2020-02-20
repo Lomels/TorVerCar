faa318b4d859dbbe397448f1a9f8836d65d1e4d5
gmarseglia <56635012+gmarseglia@users.noreply.github.com>
2020-02-20 17:31:23 +0100

    #1: Solved smell on InputChecker

--- src/logic/controller/InputChecker.java
+++ src/logic/controller/InputChecker.java
@@ -5,6 +5,10 @@ public abstract class InputChecker {
 	private static String emailPattern = "^([a-zA-Z0-9\\.\\_\\-]+)@([a-zA-Z0-9\\.\\_\\-]+)\\.([a-zA-Z0-9\\.\\_\\-]{2,5})$";
 	private static String phonePattern = "^([\\+]*)([0-9\\ ]{10,15})$";
 	
+	private InputChecker() {
+		//must remain empty
+	}
+	
 	public static boolean checkEmail(String email) {
 		return email.matches(InputChecker.emailPattern);
 	}
