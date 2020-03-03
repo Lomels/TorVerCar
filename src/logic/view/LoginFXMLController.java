6a639f5b9abe5eeaf8910539c716127b00658ff3
Lomels <34549533+Lomels@users.noreply.github.com>
2020-03-03 14:46:38 +0100

    #24

--- src/logic/view/LoginFXMLController.java
+++ src/logic/view/LoginFXMLController.java
@@ -7,42 +7,30 @@ import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import logic.bean.LoginBean;
 import logic.controller.InputChecker;
-import logic.controller.exception.*;
 import logic.controller.LoginController;
 
 public class LoginFXMLController {
     private LoginBean userBean;
-    private LoginController control;
+    private LoginController control = new LoginController();
 
-	@FXML
-	private TextField matricola;
-	
-	@FXML
-	private TextField pwd;
-	
-	@FXML
-	private Button loginButton;
-	
-	@FXML 
-	private Text text;
-	
-	@FXML
-    private URL location;
-     
-    @FXML
-    private ResourceBundle resources;
+	@FXML private TextField matricola;
+	@FXML private TextField pwd;
+	@FXML private Button loginButton;
+	@FXML private Text text;
+	@FXML private URL location;
+    @FXML private ResourceBundle resources;
 
     
 	/* RICORDATI DI METTERE onAction="#nomeController" COME ATTRIBUTO NELL'ELEMENTO DA CONTROLLARE */
 	
 	@FXML
-	public void buttonController() throws DatabaseException, InvalidInputException{
+	public void buttonController() throws Exception{
 		InputChecker.checkUserID(matricola.getText());
 		InputChecker.checkPassword(pwd.getText());
 		userBean = createBean(matricola.getText(), pwd.getText());
-		//LoginController
-		//TODO CHIAMARE IL LOGIN CONTROLLER
-		//text.setText(matricola.getText()+pwd.getText());
+		control.login(userBean);
+		//TODO chiamare controller grafico di homepage
+		text.setText("Succesfully logged in.");
 	}
 	
 	public LoginBean createBean(String userID, String password) {
