31d918a840353e5b056cb845dcae9f5f6bde961b
gmarseglia <g.marseglia.it@gmail.com>
2020-05-11 19:25:50 +0200

    #33: added daily preferencies

---
+++ src/logic/entity/DailyPreferencies.java
@@ -0,0 +1,14 @@
+package logic.entity;
+
+public class DailyPreferencies {
+
+	private Integer maxKilometer;
+	private Integer maxTime;
+	private Route startingRoute;
+
+	public DailyPreferencies(Integer maxKilometer, Integer maxTime, Route startingRoute) {
+		this.maxKilometer = maxKilometer;
+		this.maxTime = maxTime;
+		this.startingRoute = startingRoute;
+	}
+}
