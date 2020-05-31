05f70cd1a30097d5175334cacc0be9b9baba98d6
Giulia <giuls.desideri@gmail.com>
2020-05-31 19:24:07 +0200

    #7

--- src/logic/controller/ProfileController.java
+++ src/logic/controller/ProfileController.java
@@ -1,5 +1,15 @@
 package logic.controller;
 
 public class ProfileController {
+	private MySqlDAO ourDb = new MySqlDAO();
+	UserSingleton sg = UserSingleton.getInstance();
+	
+	
+	
+	public void edit(UderID userID, String email, String phone, String password) throws InvalidInputException, DatabaseException, InvalidStateException{
+		
+			 
+			 ourDb.editInfoByUserID(userID, email, phone, password);
+			 
 
-}
+	}
