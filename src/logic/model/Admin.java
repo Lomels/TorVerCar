58587cb576e881289e8a6165a2ff30855cd7e971
gmarseglia <g.marseglia.it@gmail.com>
2020-08-07 18:18:49 +0200

    #44: input validation moved into model

--- src/logic/model/Admin.java
+++ src/logic/model/Admin.java
@@ -5,27 +5,27 @@ import java.util.*;
 import logic.controller.exception.InvalidInputException;
 import logic.utilities.InputChecker;
 
-public class Admin{
-	//TODO: Implement class
-	
+public class Admin {
+
 	private String adminID;
 	private String email;
 	private String password;
 	private ArrayList<Report> handledReports;
-	
-	protected Admin(String userID, String password) throws InvalidInputException {
-		this.adminID = userID;
-		this.password = password;
+
+	protected Admin(String id, String password) throws InvalidInputException {
+		this.setAdminID(id);
+		this.setPassword(password);
 	}
-	
+
 	public String getAdminID() {
 		return this.adminID;
 	}
-	
-	public void setAdminID(String id) {
+
+	public void setAdminID(String id) throws InvalidInputException {
+		InputChecker.checkUserID(id);
 		this.adminID = id;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
@@ -34,21 +34,19 @@ public class Admin{
 		InputChecker.checkPassword(password);
 		this.password = password;
 	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
+
 	public String getEmail() {
 		return this.email;
 	}
 
-	public void addReport(Report report) {
+	public void setEmail(String email) throws InvalidInputException {
+		InputChecker.checkEmail(email);
+		this.email = email;
+	}
+
+	public void addReport(Report report) throws InvalidInputException {
+		InputChecker.checkNotNull(report, "Report");
 		this.handledReports.add(report);
 	}
-	
-	
-	
 
 }
-
