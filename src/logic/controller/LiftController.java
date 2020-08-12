4440ce51dbae736ec69a29eedeecb157d37eb8c9
gmarseglia <g.marseglia.it@gmail.com>
2020-08-12 17:30:00 +0200

    #53: implemented first version of lift match

--- src/logic/controller/LiftController.java
+++ src/logic/controller/LiftController.java
@@ -3,29 +3,85 @@ package logic.controller;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
+import logic.controller.exception.InvalidInputException;
+import logic.controller.maps.AdapterMapsApi;
+import logic.controller.maps.MapsApi;
 import logic.model.Lift;
+import logic.model.Position;
 import logic.model.Route;
 import logic.view.mysql.MySqlDAO;
 
 public class LiftController {
-	
-	private final static Integer MAX_LIFTS_LISTED = 4;
 
-	public List<Lift> matchLiftStartingAfter(Route route, LocalDateTime startDateTime, Integer initIndex) {
-		List<Lift> result = new ArrayList<Lift>();
+	private final static Integer MAX_LIFTS_LISTED = 10;
+
+	private class UnorderedLift implements Comparable<UnorderedLift> {
+
+		protected Lift lift;
+		protected Integer deltaDuration;
+
+		public UnorderedLift(Lift lift, Integer deltaDuration) {
+			this.lift = lift;
+			this.deltaDuration = deltaDuration;
+		}
+
+		@Override
+		public int compareTo(UnorderedLift o) {
+			return this.deltaDuration - o.deltaDuration;
+		}
+
+		public Lift getLift() {
+			return this.lift;
+		}
+
+	}
+
+	public List<Lift> matchLiftStartingAfter(LocalDateTime startDateTime, List<Position> stops, Integer initIndex) {
+		List<Lift> matchedLifts = new ArrayList<Lift>();
+
+		Set<UnorderedLift> unorderedLifts = new TreeSet<>();
 		MySqlDAO dao = new MySqlDAO();
+		MapsApi maps = AdapterMapsApi.getInstance();
 
+		// Get all the lifts starting after startDateTime
 		List<Lift> possibleLifts = dao.listAvailableLiftStartingAfterDateTime(startDateTime);
 
-		if (possibleLifts.size() == 0)
-			return result;
+		// For cycle that stops once it reaches the end of possibileLifts or after
+		// MAX_LIFTS_LISTED iterations
+		for (Integer index = initIndex; (index < possibleLifts.size()) && (index < MAX_LIFTS_LISTED); index++) {
+			Lift possibileLift = possibleLifts.get(index);
+			Integer currentMaxDuration = possibileLift.getMaxDuration();
+			Route currentRoute = possibileLift.getRoute();
+			Integer currentDuration = currentRoute.getDuration();
+
+			// Add all the possibile routes and order them
+			try {
+				// Compute the route passing for the positions given in stops
+				Route newRoute = maps.addInternalRoute(currentRoute, stops);
+				// If the newRoute has a duration less than the maxDuration, it is considered a
+				// match
+				if (newRoute.getDuration() <= currentMaxDuration) {
+					possibileLift.setRoute(newRoute);
+					Integer deltaDuration = newRoute.getDuration() - currentDuration;
+					unorderedLifts.add(new UnorderedLift(possibileLift, deltaDuration));
+				}
+			} catch (InvalidInputException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		// Ordered list of routes by smaller variation of duration
+		for (UnorderedLift ur : unorderedLifts) {
+			matchedLifts.add(ur.getLift());
+		}
+
+		return matchedLifts;
 		
-		
-		
-		return result;
+		// TODO: test
 	}
-	
-	
 
 }
--- src/test/LiftMatchTest.java
+++ src/test/LiftMatchTest.java
@@ -1,5 +1,42 @@
 package test;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import logic.controller.exception.DatabaseException;
+import logic.controller.exception.InvalidInputException;
+import logic.controller.maps.AdapterMapsApi;
+import logic.controller.maps.MapsApi;
+import logic.model.Lift;
+import logic.model.Route;
+import logic.model.Student;
+import logic.model.StudentCar;
+import logic.view.mysql.MySqlDAO;
+
 public class LiftMatchTest {
+
+	MySqlDAO dao = new MySqlDAO();
+	MapsApi maps = AdapterMapsApi.getInstance();
+
+	private static final String MARCO_ID = "0241118";
+
+	@Test
+	public void liftMatch() throws DatabaseException, InvalidInputException {
+		
+		Integer liftID = null;
+		LocalDateTime startDateTime = LocalDateTime.parse("2020-08-12T19:00:00");
+		Integer maxDuration = 120;
+		String note = "Fino a due ore";
+		StudentCar driver = dao.loadStudentCarByUserID(MARCO_ID);
+		List<Student> passengers = new ArrayList<Student>();
+		
+		// TODO
+		Route route = null;
+		
+		Lift liftToSave = new Lift(liftID, startDateTime, maxDuration, note, driver, passengers, route);
+	}
 
 }
