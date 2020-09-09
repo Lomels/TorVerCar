800f08dd5dd21aa65a844de1e9a41bfb12522be0
Lomels <marco.lomele@gmail.com>
2020-09-09 22:31:52 +0200

    #20

--- src/logic/view/booking/BookView.java
+++ src/logic/view/booking/BookView.java
@@ -41,6 +41,7 @@ import logic.controller.LiftMatchListener;
 import logic.controller.LoginController;
 import logic.controller.exception.ExceptionHandler;
 import logic.controller.exception.NoLiftAvailable;
+import logic.controller.exception.PassengerException;
 
 public class BookView extends Application implements Initializable, LiftMatchListener {
 	@FXML
